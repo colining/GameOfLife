e45058a48132fbbf9c9db57d0af0ffb62eec354c
colining <18810782954@163.com>
2017-06-03 10:28:55 +0800

    添加：设置细胞的生死

--- src/test/java/colining/cn/PetriTest.java
+++ src/test/java/colining/cn/PetriTest.java
@@ -1,18 +1,39 @@
 package colining.cn;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PetriTest {
+    private int size = 5;
+    private Petri petri;
+
+    @Before
+    public void setUp() {
+        petri = new Petri(size);
+    }
+
     @Test
     public void testCreate() {
-        int size = 5;
-        Petri petri = new Petri(size);
         assertEquals(size, petri.getSize());
         assertFalse(petri.isLiving(1, 1));//左上角
         assertFalse(petri.isLiving(1, size));//右上角
         assertFalse(petri.isLiving(size, 1));//左下角
         assertFalse(petri.isLiving(size, size));//右下角
+
+    }
+
+    @Test
+    public void testSet() {
+        int row = 1;
+        int column = 1;
+
+        petri.setLiving(row, column);
+        assertTrue(petri.isLiving(row, column));
+
+        petri.setDead(row, column);
+        assertFalse(petri.isLiving(row, column));
+
     }
 }
